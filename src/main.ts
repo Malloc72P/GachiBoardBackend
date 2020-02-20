9e2222fb0f03b99a0b51ed28c11a26cf330f8f80
malloc72p <dase1102@gmail.com>
2020-02-20 16:20:55 +0900

    변경내용 : 서버 main.ts의 포트에서 서버세팅파일 가져다 쓰지 않던 오류 수정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ServerSetting } from './Config/server-setting';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5200);
+  await app.listen(ServerSetting.nestPort);
 }
 bootstrap();
