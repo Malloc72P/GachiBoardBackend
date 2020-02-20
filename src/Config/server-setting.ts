5e851554e090cfac47908048e2bb4af3689ca18d
malloc72p <dase1102@gmail.com>
2020-02-20 15:38:25 +0900

    변경내용 : 백엔드 서버 배포
    1) 배포를 위한 포트 및 IP주소 수정

--- src/Config/server-setting.ts
+++ src/Config/server-setting.ts
@@ -1,18 +1,18 @@
 export class ServerSetting {
-  public static readonly serverDomain  =   "gachiboard.iptime.org";
-  public static readonly dbServerDomain  =   "localhost";
+  public static readonly serverDomain  =   "skynet765.iptime.org";
+  public static readonly dbServerDomain  =   "skynet765.iptime.org";
   public static readonly serverProtocol  =   "http://";
 
   public static readonly dbDomain  =   "mongodb://" + ServerSetting.dbServerDomain;
-  public static readonly dbPort    =   ":27017";
+  public static readonly dbPort    =   ":44199";
   public static readonly dbName    =   "/Gachiboard";
 
 
   public static readonly ngDomain  =   ServerSetting.serverProtocol + ServerSetting.serverDomain;
-  public static readonly ngPort    =   ":4200";
+  public static readonly ngPort    =   ":44172";
 
   public static readonly nestDomain  =   ServerSetting.serverProtocol + ServerSetting.serverDomain;
-  public static readonly nestPort    =   ":5200";
+  public static readonly nestPort    =   ":44174";
 
   public static readonly dbUrl    = ServerSetting.dbDomain + ServerSetting.dbPort + ServerSetting.dbName;
   public static readonly ngUrl    = ServerSetting.ngDomain + ServerSetting.ngPort;
