dff429cdf2082c5eda696f87267ccf23200b7d98
orange0909 <orange0909@icloud.com>
2020-02-23 23:04:13 +0900

    변경내용 : 서버 설정 json으로 추출
    1. 제곧내

--- src/Config/server-setting.ts
+++ src/Config/server-setting.ts
@@ -1,25 +1,24 @@
+import * as serverSecretJson from "./serverSecret.json";
+
 export class ServerSetting {
-  public static readonly serverDomain  =   "gachiboard.iptime.org";
-  public static readonly dbServerDomain  =   "localhost";
-  public static readonly serverProtocol  =   "http://";
+  public static readonly serverProtocol   = serverSecretJson.serverProtocol;
+  public static readonly dbProtocol       = serverSecretJson.dbProtocol;
 
-  public static readonly dbDomain  =   "mongodb://" + ServerSetting.dbServerDomain;
-  public static readonly dbPort    =   ":27017";
-  public static readonly dbName    =   "/Gachiboard";
+  public static readonly frontendDomain   = serverSecretJson.frontendDomain;
+  public static readonly backendDomain    = serverSecretJson.backendDomain;
 
+  public static readonly ngPort           = serverSecretJson.ngPort;
+  public static readonly nestPort         = serverSecretJson.nestPort;
+  public static readonly dbPort           = serverSecretJson.dbPort;
 
-  public static readonly ngDomain  =   ServerSetting.serverProtocol + ServerSetting.serverDomain;
-  public static readonly ngPort    =   ":4200";
+  public static readonly dbName           = serverSecretJson.dbName;
 
-  public static readonly nestDomain  =   ServerSetting.serverProtocol + ServerSetting.serverDomain;
-  public static readonly nestPort    =   ":5200";
+  public static readonly ngUrl            = ServerSetting.serverProtocol + ServerSetting.frontendDomain + ServerSetting.ngPort;
+  public static readonly dbUrl            = ServerSetting.dbProtocol + ServerSetting.backendDomain + ServerSetting.dbPort;
+  public static readonly nestUrl          = ServerSetting.serverProtocol + ServerSetting.backendDomain + ServerSetting.nestPort;
 
-  public static readonly dbUrl    = ServerSetting.dbDomain + ServerSetting.dbPort + ServerSetting.dbName;
-  public static readonly ngUrl    = ServerSetting.ngDomain + ServerSetting.ngPort;
-  public static readonly nestUrl  = ServerSetting.nestDomain + ServerSetting.nestPort;
-
-  public static readonly callbackURL  = ServerSetting.nestUrl
-                                        + "/auth/google/callback";
+  public static readonly callbackURL      = ServerSetting.nestUrl
+                                              + "/auth/google/callback";
 
   public static readonly ngRoutes = {
     "loginSuccess" : ServerSetting.ngUrl + "/login/success/",
